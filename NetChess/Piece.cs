e209830ab379b80b5a62272c22fef99b9eb9bcd9
OliWan9 <oliver.hitz.7@gmail.com>
2025-12-18 12:23:14 +0100

    class Piece

--- NetChess/Piece.cs
+++ NetChess/Piece.cs
@@ -2,5 +2,8 @@ namespace NetChess;
 
 public class Piece
 {
+    public (int, int) coordinates; // x, y
     
+    public enum Type { Pawn, Bishop, Knight, Rook, Queen, King }
+    public Type pieceType;
 }
